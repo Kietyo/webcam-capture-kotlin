f3e03400a32af5fb19ca609d7cebee3f8a0d55c4
Xeno <xenotactic.game@gmail.com>
2022-11-14 20:54:59 -0800

    Working on productionizing.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,5 @@
 import com.github.sarxos.webcam.Webcam
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
@@ -7,25 +8,50 @@ import java.time.LocalDateTime
 import java.util.TimeZone
 import kotlin.time.TimeSource
 
-fun main() {
-//    println("Hello world")
+private var numCoroutines = 0
 
-    runBlocking {
-        launch {
-            var i = 0
-            while (true) {
-                i++
-                val time = LocalDateTime.now()
-                println("Running $i: ${LocalDateTime.now()}")
-                val webcamNames = Webcam.webcams
-                println(webcamNames)
-                println()
-                delay(2000)
-            }
-        }
+fun main() {
+    println("Hello world")
+    launchCoroutine()
+    launchCoroutine()
+    launchCoroutine()
+    launchCoroutine()
+
+//    runBlocking {
+//
+//    }
+    while(true) {
+
     }
 
+//    runBlocking {
+//        launch {
+//            var i = 0
+//            while (true) {
+//                i++
+//                val time = LocalDateTime.now()
+//                println("Running $i: ${LocalDateTime.now()}")
+//                val webcamNames = Webcam.webcams
+//                println(webcamNames)
+//                println()
+//                delay(2000)
+//            }
+//        }
+//    }
+}
 
-
-
+fun launchCoroutine() {
+    GlobalScope.launch {
+        val id = numCoroutines++
+        var i = 0
+        while (true) {
+            i++
+            val time = LocalDateTime.now()
+            println("$id: Running $i: ${LocalDateTime.now()}")
+            val webcamNames = Webcam.webcams
+            println(webcamNames)
+            println()
+            delay(2000)
+        }
+    }
 }
