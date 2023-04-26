c359ff4f1c09fe10ec9d0057f78e8548ce328d12
Xeno <xenotactic.game@gmail.com>
2023-04-25 21:56:21 -0700

    Migrating to Kotlin 1.8.21 and Korge 4.0 RC

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    val kotlinVersion: String by settings
+    val kotlinxBenchmark: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+    }
+}
 
 rootProject.name = "webcam-capture-kotlin"
 
